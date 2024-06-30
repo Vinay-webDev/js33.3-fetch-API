f6b2e2427ff9efc27cac003ddc8bcee0d885e84e
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-30 15:40:31 +0530

    1.

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// fetch API
+// Async/Await
+
+// when using async we need to tell right at the begining 
+/*
+async function myFunction() {
+
+} */
+//===================//--------------------------//
+const myUsers = {
+    usersLists: [],
+}
+const myFunction = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const jsonUserData = await response.json();
+    console.log(jsonUserData);
+    return jsonUserData;
+}
+
+myFunction();
+
+
+
+
+
+
+
+
+
+
+
+
